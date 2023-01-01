af6b09a37f0bc8ebee72483389712b4b668489ab
balinux <ryojuniyantara@gmail.com>
2023-01-01 17:53:19 +0800

    update order andpayemnt log

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $table = 'orders';
+    protected $fillable = [
+        'status', 'user_id', 'course_id', 'metadata', 'snap_url'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:m:s',
+        'metadata' => 'array'
+    ];
 }
