8fbeb57ae37263110ae156ea775ab0a5f6d07d8a
balinux <ryojuniyantara@gmail.com>
2023-01-01 19:44:22 +0800

    API Crete Order wih midtrans

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,9 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class OrderController extends Controller
 {
+
+    /**
+     * get midtrans snap url
+     * 
+     */
+
+
+    private function getMidtransSnapurl($params)
+    {
+        \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        \Midtrans\Config::$isProduction = (bool) env('MIDTRANS_PRODUCTION');
+        \Midtrans\Config::$is3ds = (bool) env('MIDTRANS_3DS');
+
+        $snapUrl = \Midtrans\Snap::createTransaction($params)->redirect_url;
+        return $snapUrl;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -35,7 +52,67 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = $request->input('user');
+        $course = $request->input('course');
+
+        $order = Order::create([
+            'user_id' => $user['id'],
+            'course_id' => $course['id'],
+        ]);
+
+        // transaction detail
+
+        $transsactionDetail = [
+            'order_id' => $order->id.Str::random(5),
+            'gross_amount' => $course['price'],
+        ];
+
+        $itemDetails = [
+            [
+                'id' => $course['id'],
+                'price' => $course['price'],
+                'quantity' => 1,
+                'name' => $course['name'],
+                'brand' => 'yhotie',
+                'category' => 'online course'
+            ]
+        ];
+
+        $customerDetail = [
+            'first_name' => $user['name'],
+            'email' => $user['email']
+        ];
+        /**
+         * membuat parameter untuk mendapatkan snap Url
+         * 
+         */
+        $midtransParams = [
+            'transaction_details' => $transsactionDetail,
+            'item_details' => $itemDetails,
+            'customer_details' => $customerDetail,
+
+        ];
+
+        // generate snap url
+        $midtransSnapUrl = $this->getMidtransSnapurl($midtransParams);
+
+        // update order
+        $order->snap_url = $midtransSnapUrl;
+
+        $order->metadata = [
+            'course_id' => $course['id'],
+            'course_price' => $course['price'],
+            'course_name' => $course['name'],
+            'course_thumbnail' => $course['thumbnail'],
+            'course_level' => $course['level'],
+        ];
+
+        $order->save();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $order
+        ]);
     }
 
     /**
